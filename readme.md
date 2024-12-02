2612459bebe293f83ed0f1097d208f98cfe507d7
Tim Zolleis <tim@zolleis.net>
2024-12-02 23:05:16 +0100

    docs(workflow): add documentation

---
+++ readme.md
@@ -0,0 +1,75 @@
+# ts-workflow
+Define safe workflows in TypeScript with ease.
+
+
+## Installation
+```bash
+npm install ts-workflow
+```
+
+## Usage
+
+### Defining a step
+Define your workflow steps using the `defineStep` helper function.
+
+
+```typescript
+import {defineStep} from 'ts-workflow';
+
+const myStep = defineStep('myStep', async (input: string) => {
+    console.log(input);
+    return input
+});
+```
+### Defining a workflow
+After defining a step, you can create a workflow with the `defineWorkflow` helper.
+
+```typescript
+import {defineWorkflow} from 'ts-workflow';
+import {myStep} from "./myStep";
+
+const myWorkflow = defineWorkflow({
+    steps: [myStep],
+    name: 'myWorkflow'
+});
+```
+
+### Running your workflow
+You can run your workflow by calling the `run` method on the workflow instance.
+This produces a promise that resolves to a `Result`
+
+```typescript
+const workflowResult = await myWorkflow.run();
+if (workflowResult.isOk) {
+    console.log('Workflow ran successfully');
+} else {
+    console.log('Workflow failed', workflowResult.error);
+}
+```
+
+### Passing initial data to your workflow
+The workflow's initial data is handled in its **context**. You can setup this context by defining a `setupContext` function, which arguments must be passed in the run method of the workflow
+
+```typescript
+import {defineWorkflow, createContext} from 'ts-workflow';
+
+const nameContext = createContext<string>();
+
+const myWorkflow = defineWorkflow({
+    steps: [myStep],
+    name: 'myWorkflow',
+    setupContext: (name: string, ctx) => {
+        ctx.set(nameContext, name);
+    }
+});
+
+const workflowResult = await myWorkflow.run('Hello, World!');
+
+```
+
+### Implementing a storage strategy for database-backing your workflow
+You can provide a `StorageStrategy` to your workflow to persist the state of your workflow.
+
+// In progress
+
+
--- src/workflow.ts
+++ src/workflow.ts
@@ -1,20 +1,29 @@
-import {StorageStrategy} from "./storage-strategy";
-import {WorkflowStep} from "./step";
-import {WorkflowContext} from "./context";
 import {successResult} from "./result";
+import {StorageStrategy} from "./storage-strategy";
+import {WorkflowContext} from "./context";
+import {WorkflowStep} from "./step";
 
 type WorkflowStatus = "pending" | "running" | "completed" | "failed";
 
-abstract class BaseWorkflow {
+interface DefineWorkflowOptions<IDType, T = never> {
+    name: string;
+    steps: WorkflowStep<unknown, unknown>[];
+    storageStrategy?: StorageStrategy<IDType>;
+    setupContext?: (data: T, context: WorkflowContext) => void;
+}
+
+class BaseWorkflow<IDType, T = never> {
     protected readonly name: string;
     protected readonly context: WorkflowContext;
     protected readonly steps: WorkflowStep<unknown, unknown>[];
+    protected setupContext?: (data: T, context: WorkflowContext) => void;
     protected currentStepIndex = 0;
 
-    constructor(options: DefineWorkflowOptions<unknown>) {
+    constructor(options: DefineWorkflowOptions<IDType, T>) {
         this.name = options.name;
         this.steps = options.steps;
-        this.context = options.setupContext ? options.setupContext(new WorkflowContext()) : new WorkflowContext();
+        this.context = new WorkflowContext();
+        this.setupContext = options.setupContext;
     }
 
     protected async rollback() {
@@ -27,64 +36,61 @@ abstract class BaseWorkflow {
     }
 }
 
+class StoredWorkflow<IDType, T = never> extends BaseWorkflow<IDType, T> {
+    private readonly storageStrategy: StorageStrategy<IDType>;
 
-class StoredWorkflow<IDType> extends BaseWorkflow {
-    private readonly storageStrategy: StorageStrategy<IDType>
-
-    constructor(options: DefineWorkflowOptions<IDType> & { storageStrategy: StorageStrategy<IDType> }) {
+    constructor(options: DefineWorkflowOptions<IDType, T> & { storageStrategy: StorageStrategy<IDType> }) {
         super(options);
         this.storageStrategy = options.storageStrategy;
     }
 
-    public async run() {
+    public async run(data?: T) {
+        if (this.setupContext && data) {
+            this.setupContext(data, this.context);
+        }
         const workflowId = await this.storageStrategy.storeWorkflow(this.name);
         for (const step of this.steps) {
-            const stepId = await this.storageStrategy.storeStep({workflowId, stepName: step.name});
+            const stepId = await this.storageStrategy.storeStep({ workflowId, stepName: step.name });
             const stepResult = await step.run(this.context);
             if (stepResult.isErr) {
-                await Promise.all([this.storageStrategy.updateStepStatus({
-                    stepId,
-                    status: "failed",
-                    workflowId
-                }), this.storageStrategy.updateWorkflowStatus({workflowId, status: "failed"})]);
+                await Promise.all([
+                    this.storageStrategy.updateStepStatus({ stepId, status: "failed", workflowId }),
+                    this.storageStrategy.updateWorkflowStatus({ workflowId, status: "failed" })
+                ]);
                 await this.rollback();
-                return {...stepResult, stepName: step.name};
+                return { ...stepResult, stepName: step.name };
             }
-            await this.storageStrategy.updateStepStatus({stepId, status: "completed", workflowId});
+            await this.storageStrategy.updateStepStatus({ stepId, status: "completed", workflowId });
             this.currentStepIndex++;
         }
-        await this.storageStrategy.updateWorkflowStatus({workflowId, status: "completed"});
-        return successResult({id: workflowId});
+        await this.storageStrategy.updateWorkflowStatus({ workflowId, status: "completed" });
+        return successResult({ id: workflowId });
     }
 }
 
-class InMemoryWorkflow extends BaseWorkflow {
-    public async run() {
+class InMemoryWorkflow<T = never> extends BaseWorkflow<unknown, T> {
+    public async run(data?: T) {
+        if (this.setupContext && data) {
+            this.setupContext(data, this.context);
+        }
         for (const step of this.steps) {
             const stepResult = await step.run(this.context);
             if (stepResult.isErr) {
                 await this.rollback();
-                return {...stepResult, stepName: step.name};
+                return { ...stepResult, stepName: step.name };
             }
             this.currentStepIndex++;
         }
-        return successResult({id: 1});
+        return successResult({ id: 1 });
     }
 }
 
-interface DefineWorkflowOptions<IDType> {
-    name: string;
-    steps: WorkflowStep<unknown, unknown>[];
-    storageStrategy?: StorageStrategy<IDType>;
-    setupContext?: (context: WorkflowContext) => WorkflowContext;
-}
-
-function defineWorkflow<IDType>(options: DefineWorkflowOptions<IDType>) {
+function defineWorkflow<IDType, T = never>(options: DefineWorkflowOptions<IDType, T>) {
     const storageStrategy = options.storageStrategy;
     if (storageStrategy) {
-        return new StoredWorkflow({...options, storageStrategy});
+        return new StoredWorkflow({ ...options, storageStrategy });
     }
     return new InMemoryWorkflow(options);
 }
 
-export {defineWorkflow, WorkflowStatus, BaseWorkflow};
+export {defineWorkflow, WorkflowStatus};
